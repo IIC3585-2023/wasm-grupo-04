8a5215bd21d6db8ee47936ee72584ce98cfcb7e8
alfre <alfredo.medina@uc.cl>
2023-04-23 16:40:21 -0400

    feat: C solver

---
+++ functions/solver.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// insertion sort descending
+void insertionSort(int jobs[], int n_jobs)
+{
+    int i, key, j;
+    for (i = 1; i < n_jobs; i++) {
+        key = jobs[i];
+        j = i - 1;
+        while (j >= 0 && jobs[j] < key) {
+            jobs[j + 1] = jobs[j];
+            j = j - 1;
+        }
+        jobs[j + 1] = key;
+    }
+}
+
+
+int bestFit(int jobs[], int n_jobs, int n_clusters)
+{
+    // sort jobs in descending order
+    insertionSort(jobs, n_jobs);
+ 
+    int clusters[n_clusters];
+    // initialize all clusters to 0
+    for (int c = 0; c < n_clusters; c++) {
+        clusters[c] = 0;
+    }
+ 
+    // assign each job to the cluster with the least seconds
+    for (int j = 0; j < n_jobs; j++) {
+        int best_cluster = 0;
+        for (int c = 0; c < n_clusters; c++) {
+            if (clusters[c] < clusters[best_cluster]) {
+                best_cluster = c;
+            }
+        }
+        clusters[best_cluster] += jobs[j];
+    }
+
+    int total_seconds = 0;
+
+    // look for the cluster with the most seconds
+    for (int c = 0; c < n_clusters; c++) {
+        if (clusters[c] > total_seconds) {
+            total_seconds = clusters[c];
+        }
+    }
+
+    return total_seconds;
+}
+
+int main()
+{
+    int jobs[] = { 30, 50, 10, 20, 90};
+    int n_jobs = sizeof(jobs) / sizeof(jobs[0]);
+    int n_clusters = 2;
+    printf("Min Seconds: %d\n", bestFit(jobs, n_jobs, n_clusters));
+    return 0;
+}
+
+
+// gcc solver.c -o solver
+// ./solver
