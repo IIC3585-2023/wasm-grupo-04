9f9ed55f5a25da3e170113afde9cb41cbed66c42
alfre <alfredo.medina@uc.cl>
2023-04-24 21:51:25 -0400

    fix wasmWorkers

--- functions/wasm_c_insertion/wasmWorker.js
+++ functions/wasm_c_insertion/wasmWorker.js
@@ -2,10 +2,19 @@ importScripts("cSolver.js");
 
 this.onmessage = (m) => {
   Module.onRuntimeInitialized = () => {
-    const solver = Module.cwrap("c_solver", "number", ["array", "number"]);
-    const typedArray = new Int32Array(m.data.jobs);
+    const c_solver = Module.cwrap("c_solver", "number", [
+      "number",
+      "number",
+      "number",
+    ]);
+    let typedArray = new Int32Array(m.data.jobs);
+    let pointer = Module._malloc(
+      typedArray.length * typedArray.BYTES_PER_ELEMENT
+    );
+    Module.HEAP32.set(typedArray, pointer / typedArray.BYTES_PER_ELEMENT);
 
-    const result = solver(typedArray, m.data.n_clusters);
+    const result = c_solver(pointer, typedArray.length, m.data.n_clusters);
+    Module._free(pointer);
     this.postMessage(result);
   };
 };
