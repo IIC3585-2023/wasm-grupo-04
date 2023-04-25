f70f570ac13e879722c21c09c36a68e6e890ef7c
Rocio Torres <rocio.tp@uc.cl>
2023-04-24 21:57:09 -0400

    quick sort

--- functions/solver.c
+++ functions/solver.c
@@ -119,7 +119,7 @@ int main()
 {
     int jobs[] = { 30, 50, 10, 20, 90};
     int n_jobs = sizeof(jobs) / sizeof(jobs[0]);
-    int n_clusters = 100;
+    int n_clusters = 2;
     printf("Min Seconds: %d\n", c_solver(jobs, n_jobs, n_clusters));
     printf("Min Seconds: %d\n", c_solver_qs(jobs, n_jobs, n_clusters));
     return 0;
